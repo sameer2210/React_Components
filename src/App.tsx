1e1b790e9655201afd46cb7263edc1cbf1034231
SAM <sameerkhan27560@gmail.com>
2025-08-16 22:31:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+     Sam React Componenets
+    </div>
+  );
+}
+
+export default App;
